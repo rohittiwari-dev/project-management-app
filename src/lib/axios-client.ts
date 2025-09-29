9f9f756c3e3ef426e4cfa3f6fa4ae0c5ceb93504
Rohit Tiwari <tigertiwari1023@gmail.com>
2025-09-29 17:07:20 +0530

    fix :  for netlify

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -1,7 +1,7 @@
 import { CustomError } from "@/types/custom-error.type";
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 const options = {
   baseURL,
